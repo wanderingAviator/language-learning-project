3345d0aab0b46f1b627ab45030cd345763b58780
Albert <albertzeap@gmail.com>
2023-09-12 08:27:06 -0700

    Created backend skeleton

---
+++ backend/language-learning/src/main/java/com/cognixia/jump/languagelearning/LanguageLearningApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.languagelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearningApplication.class, args);
+	}
+
+}
