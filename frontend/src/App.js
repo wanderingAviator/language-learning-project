cc7b6a1a050cb4508c55d6919972ba1483f6cc2e
Kelly Ortega <InvaderLynn10@gmail.com>
2023-09-13 10:09:45 -0400

    started work on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Login from './components/Login/Login';
+import Landing from './components/Landing/Landing';
+import Signup from './components/Signup/Signup';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Signup = () => {
+  return (
+    <div className="signup-container">
+      <Link to='/'>Back to Home</Link>
+    </div>
+  )
+}
+
+export default Signup
