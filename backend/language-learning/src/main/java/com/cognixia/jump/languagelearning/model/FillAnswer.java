f8b929fc5c67e1a47e2f83c48a3af27a97342b24
Dominic Swaby <domswaby@gmail.com>
2023-09-13 11:20:40 -0400

    make Topic, FillAnswer, and FillQuestion models

---
+++ backend/language-learning/src/main/java/com/cognixia/jump/languagelearning/model/FillAnswer.java
@@ -0,0 +1,62 @@
+package com.cognixia.jump.languagelearning.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class FillAnswer {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Integer id;
+	
+	@NotBlank
+	private String answer;
+	
+	@ManyToOne
+	@JoinColumn( name = "question_id", referencedColumnName = "id" ) // can add nullable = false to make sure dorm id is given for each account (won't b/c we may have commuter students)
+	private FillQuestion question;
+	
+	public FillAnswer(Integer id, String answer, FillQuestion question) {
+		super();
+		this.id = id;
+		this.answer = answer;
+		this.question = question;
+	}
+	
+	public FillAnswer() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public FillQuestion getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(FillQuestion question) {
+		this.question = question;
+	}
+	
+	
+}
