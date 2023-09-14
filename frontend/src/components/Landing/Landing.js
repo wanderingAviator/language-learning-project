89ea828395c97c1e8eea64f8a24e637f2f8967be
Kelly Ortega <InvaderLynn10@gmail.com>
2023-09-14 15:23:41 -0400

    finished signup page part 2

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -11,8 +11,11 @@ const Landing = () => {
       <div className="text-container">
         <h1>BIG OL TITLE</h1>
         <p>this is a catchy, super-fancy tagline yaaaaaay wait what happens it its super long</p>
-        <a href="http://localhost:3000/login"><button >Login</button></a> <br/>
-        <a href="http://localhost:3000/signup"><button>Signup</button></a>
+
+        <div className="landing-buttons">
+          <a href="http://localhost:3000/login"><button >Login</button></a>
+          <a href="http://localhost:3000/signup"><button>Signup</button></a>
+        </div>
       </div>
         
       <div className="image-container">
