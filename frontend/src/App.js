327d8882d20c96e6f59ab9c2f5b8fa03eff26303
Sarah Ortega <snortega1999@gmail.com>
2023-09-18 12:27:52 -0400

    finished page templates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import Login from './components/Login/Login';
 import Landing from './components/Landing/Landing';
 import Signup from './components/Signup/Signup';
 import Main from './components/Mainpage/MainPage';
+import Matching from './components/Matching/Matching';
+import Fitb from './components/Fitb/Fitb';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
@@ -13,6 +15,8 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/main' element={<Main/>}/>
+        <Route path='/language/matching' element={<Matching/>}/>
+        <Route path='/language/fitb' element={<Fitb/>}/>
 
       </Routes>
     </div>
