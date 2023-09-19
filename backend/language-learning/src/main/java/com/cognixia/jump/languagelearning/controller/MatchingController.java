ef83f1db7c54d3b299dd1c6e672248a69be9bb71
Albert <albertzeap@gmail.com>
2023-09-19 11:10:19 -0700

    Created random matching question endpoint

--- backend/language-learning/src/main/java/com/cognixia/jump/languagelearning/controller/MatchingController.java
+++ backend/language-learning/src/main/java/com/cognixia/jump/languagelearning/controller/MatchingController.java
@@ -1,6 +1,5 @@
 package com.cognixia.jump.languagelearning.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.languagelearning.exception.ResourceNotFoundException;
 import com.cognixia.jump.languagelearning.model.MatchingQuestion;
 import com.cognixia.jump.languagelearning.service.MatchingService;
 
@@ -21,17 +21,11 @@ public class MatchingController {
 	@Autowired
 	MatchingService matchingService;
 	
-	@GetMapping("/match_card/language/{languageId}")
-	public ResponseEntity<?> getQuestionsByLanguage(@PathVariable int languageId){
+	@GetMapping("/matching/{languageId}")
+	public ResponseEntity<?> getQuestionsByLanguage(@PathVariable int languageId) throws ResourceNotFoundException{
 		
-		List<MatchingQuestion> question = matchingService.getQuestionsByLanguage(languageId);
+		MatchingQuestion question = matchingService.getQuestionsByLanguage(languageId);
 		return ResponseEntity.status(HttpStatus.OK).body(question);
 	}
 	
-	@GetMapping("/match_card/{id}")
-	public ResponseEntity<?> getQuestionById(@PathVariable int id){
-		
-		MatchingQuestion question = matchingService.getQuestion(id);
-		return ResponseEntity.status(HttpStatus.OK).body(question);
-	}
 }
